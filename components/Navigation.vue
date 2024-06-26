ee42468b6df74ae6291d39bc6e3f59202c719e32
Aira Sakuranomiya <aira@aira.cafe>
2024-06-27 03:28:26 +0800

    Update

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -3,6 +3,7 @@
 
 <template>
 	<div>
+		<div class="bg"></div>
 		<div class="left">
 			<NuxtLink to="/">
 				<span class="material-icons">local_cafe</span>
@@ -31,86 +32,97 @@
 </template>
 
 <style scoped lang="scss">
-	$lr-padding: 1rem;
+$lr-padding: 1rem;
 
-	.navigation {
-		--nav-left-color: var(--accent-500);
-		--nav-right-color: var(--icon);
+.navigation {
+	--nav-left-color: var(--accent-500);
+	--nav-right-color: var(--icon);
+	position: relative;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	width: 100%;
+	height: 3rem;
+	// box-shadow: 0 0 1.5rem 0 rgb(var(--accent-500-rgb) / 30%);
+
+	&.white {
+		--nav-left-color: white;
+		--nav-right-color: white;
+	}
+
+	.bg {
+		mask-image: linear-gradient(black 60%, transparent);
+		position: absolute;
+		right: 0;
+		left: 0;
+		z-index: -1;
+		height: 4rem;
+		backdrop-filter: blur(20px);
+	}
+
+	.site-name {
+		@include title-font;
+		font-weight: 600;
+		font-size: 1.5rem;
+
+		@include phone {
+			display: none;
+		}
+	}
+
+	.left {
+		height: 100%;
+
+		a {
+			display: flex;
+			flex-shrink: 0;
+			gap: 4px;
+			align-items: center;
+			justify-content: center;
+			height: 100%;
+			padding: 0 $lr-padding;
+			color: var(--nav-left-color);
+		}
+	}
+
+	.right {
 		display: flex;
 		align-items: center;
-		justify-content: space-between;
-		width: 100%;
-		height: 3rem;
-		// box-shadow: 0 0 1.5rem 0 rgb(var(--accent-500-rgb) / 30%);
+		height: 100%;
+		margin-right: 4px;
+		font-family: Montserrat, sans-serif;
 
-		&.white {
-			--nav-left-color: white;
-			--nav-right-color: white;
-		}
-
-		.site-name {
-			@include title-font;
-			font-weight: 600;
-			font-size: 1.5rem;
-
-			@include phone {
-				display: none;
-			}
-		}
-
-		.left {
-			height: 100%;
-
-			a {
-				display: flex;
-				flex-shrink: 0;
-				gap: 4px;
-				align-items: center;
-				justify-content: center;
-				height: 100%;
-				padding: 0 $lr-padding;
-				color: var(--nav-left-color);
-			}
-		}
-
-		.right {
+		a {
 			display: flex;
+			gap: 4px;
 			align-items: center;
+			justify-content: center;
 			height: 100%;
-			margin-right: 4px;
-			font-family: Montserrat, sans-serif;
+			padding: 0 12px;
+			color: var(--nav-right-color);
+			text-decoration: none;
 
-			a {
-				display: flex;
-				gap: 4px;
-				align-items: center;
-				justify-content: center;
-				height: 100%;
-				padding: 0 12px;
-				color: var(--nav-right-color);
-				text-decoration: none;
+			&::after {
+				position: absolute;
+				bottom: 0;
+				display: block;
+				width: 24px;
+				height: 2px;
+				margin-top: 3px;
+				background-color: var(--accent-500);
+				transform: scaleX(0);
+				transition: transform 0.6s;
+				content: "";
+			}
+
+			&.router-link-active {
+				color: var(--accent-500);
 
 				&::after {
-					position: absolute;
-					bottom: 0;
-					display: block;
-					width: 24px;
-					height: 2px;
-					margin-top: 3px;
-					background-color: var(--accent-500);
-					transform: scaleX(0);
-					transition: transform 0.6s;
-					content: "";
-				}
-
-				&.router-link-active {
-					color: var(--accent-500);
-
-					&::after {
-						transform: scaleX(1);
-					}
+					transform: scaleX(1);
 				}
 			}
 		}
 	}
+}
 </style>
--- pages/profile.vue
+++ pages/profile.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-	useHead({ title: "My Profile" });
+useHead({ title: "My Profile" });
 </script>
 
 <template>
@@ -51,15 +51,16 @@
 				</div>
 
 				<div>
-					<span class="material-icons">games</span>
-					<span>Minecraft</span>
+					<span class="material-icons">local_laundry_service</span>
+					<span>maimai</span>
 				</div>
 
 			</div>
 
 			<div class="side-img-wrapper">
-				<h2>Who is<br><b>Aira?</b></h2>
-				<NuxtImg src="https://m1.miaomc.cn/uploads/20230804_64cbee56379a2.webp" class="side-img" alt="A photo of me" />
+				<!-- <h2>Who is<br><b>Aira?</b></h2> -->
+				<NuxtImg src="https://m1.miaomc.cn/uploads/20230804_64cbee56379a2.webp" class="side-img"
+					alt="A photo of me" />
 			</div>
 
 		</div>
@@ -67,131 +68,132 @@
 </template>
 
 <style scoped lang="scss">
-	.page-wrapper {
-		// @include content-padding;
-		display: flex;
-		gap: 3rem;
-		height: 100vh;
+.page-wrapper {
+	// @include content-padding;
+	display: flex;
+	gap: 3rem;
+	height: 100vh;
+}
+
+.content {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	width: 100%;
+	height: 100%;
+
+	@include phone {
+		gap: 0;
+	}
+}
+
+@keyframes list-intro {
+	from {
+		transform: translateX(-100%);
+	}
+}
+
+@keyframes list-item-intro {
+	from {
+		transform: translateX(-150%);
+	}
+}
+
+@for $i from 1 through 8 {
+	.list > div:nth-of-type(#{$i}) {
+		animation-delay: calc($i / 20) + s !important;
+	}
+}
+
+.list {
+	// @include text-shadow;
+	display: flex;
+	flex-direction: column;
+	flex-shrink: 0;
+	gap: 1.5rem;
+	padding: 0 8vw;
+	color: var(--icon);
+	animation: 1s cubic-bezier(0.1, 0.9, 0.2, 1) backwards list-intro;
+
+	hr {
+		margin: 0;
+		visibility: hidden;
 	}
 
-	.content {
+	> h4 {
+		color: var(--accent-500);
+		font-size: 1.5rem;
+		text-align: center;
+
+		@include dark {
+			@include small-text-shadow;
+		}
+	}
+
+	> div {
 		display: flex;
+		gap: 0.5rem;
 		align-items: center;
-		justify-content: space-between;
-		height: 100%;
-
-		@include phone {
-			gap: 0;
-		}
+		animation: 0.6s cubic-bezier(0.1, 0.9, 0.2, 1) backwards list-item-intro;
 	}
 
-	@keyframes list-intro {
-		from {
-			transform: translateX(-100%);
-		}
-	}
-
-	@keyframes list-item-intro {
-		from {
-			transform: translateX(-150%);
-		}
-	}
-
-	@for $i from 1 through 8 {
-		.list>div:nth-of-type(#{$i}) {
-			animation-delay: calc($i / 20) + s !important;
-		}
-	}
-
-	.list {
-		// @include text-shadow;
-		display: flex;
-		flex-direction: column;
-		flex-shrink: 0;
-		gap: 1.5rem;
+	@include phone {
+		width: 50%;
 		padding: 0 8vw;
-		color: var(--icon);
-		animation: 1s cubic-bezier(0.1, 0.9, 0.2, 1) backwards list-intro;
+	}
+}
 
-		hr {
-			margin: 0;
-			visibility: hidden;
-		}
+@keyframes side-img-wrapper-intro {
+	from {
+		transform: translateX(100%);
+		// filter: blur(100rem);
+	}
+}
 
-		>h4 {
-			color: var(--accent-500);
-			font-size: 1.5rem;
-			text-align: center;
+@keyframes opacity-intro {
+	from {
+		opacity: 0;
+	}
+}
 
-			@include dark {
-				@include small-text-shadow;
-			}
-		}
+.side-img-wrapper {
+	position: relative;
+	width: 100%;
+	height: 100%;
+	animation: 1s cubic-bezier(0.1, 0.9, 0.2, 1) backwards side-img-wrapper-intro;
 
-		>div {
-			display: flex;
-			gap: 0.5rem;
-			align-items: center;
-			animation: 0.6s cubic-bezier(0.1, 0.9, 0.2, 1) backwards list-item-intro;
+	h2 {
+		position: absolute;
+		max-width: inherit;
+		color: white;
+		font-weight: 400;
+		font-size: 10vw;
+		text-shadow: 0 0.05em 0.1em rgb(var(--accent-500-rgb) / 15%);
+
+		@include tablet {
+			display: none;
 		}
 
 		@include phone {
-			width: 50%;
-			padding: 0 8vw;
+			display: none;
 		}
 	}
 
-	@keyframes side-img-wrapper-intro {
-		from {
-			transform: translateX(100%);
-			// filter: blur(100rem);
-		}
+	@include phone {
+		flex-shrink: 0;
+		width: 50%;
 	}
+}
 
-	@keyframes opacity-intro {
-		from {
-			opacity: 0;
-		}
+.side-img {
+	display: block;
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+	object-position: 80% 50%;
+
+	@include phone {
+		object-position: 62% 50%;
 	}
-
-	.side-img-wrapper {
-		position: relative;
-		width: 100%;
-		height: 100%;
-		animation: 1s cubic-bezier(0.1, 0.9, 0.2, 1) backwards side-img-wrapper-intro;
-
-		h2 {
-			position: absolute;
-			max-width: inherit;
-			color: white;
-			font-weight: 400;
-			font-size: 10vw;
-			text-shadow: 0 0.05em 0.1em rgb(var(--accent-500-rgb) / 15%);
-
-			@include tablet {
-				display: none;
-			}
-
-			@include phone {
-				display: none;
-			}
-		}
-
-		@include phone {
-			flex-shrink: 0;
-			width: 50%;
-		}
-	}
-
-	.side-img {
-		display: block;
-		width: 100%;
-		height: 100%;
-		object-fit: cover;
-		object-position: 80% 50%;
-
-		@include phone {
-			object-position: 62% 50%;
-		}
-	}
+}
 </style>
