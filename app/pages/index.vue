0ea069a14f5479bc395b5c427e0db323784456be
Aira Sakuranomiya <aira@aira.cafe>
2025-12-14 17:58:50 +0800

    Add easter egg

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,37 @@
 <script setup lang="ts">
 useHead({ title: 'Home' })
+
+const secret = 'hentai'
+let index = 0
+
+function onKeydown(e: KeyboardEvent) {
+  if (e.key.length !== 1)
+    return
+
+  if (e.key.toLowerCase() === secret[index]) {
+    index++
+
+    if (index === secret.length) {
+      index = 0
+      triggerEasterEgg()
+    }
+  }
+  else {
+    index = 0
+  }
+}
+
+function triggerEasterEgg() {
+  window.open('https://x.com/otono_rinne', '_blank')
+}
+
+onMounted(() => {
+  window.addEventListener('keydown', onKeydown)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('keydown', onKeydown)
+})
 </script>
 
 <template>
